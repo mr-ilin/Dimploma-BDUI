ee3edd3d9ea87f128b596c850eb1d564faa30f21
Ильин Илья Олегович <ioilin@domclick.ru>
2023-04-06 19:03:04 +0300

    Initial Commit

---
+++ BDUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BDUI
+//
+//  Created by Ильин Илья Олегович on 06.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
